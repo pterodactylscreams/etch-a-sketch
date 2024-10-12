c7ba80b8e88fb86d43abd43ed581d1be7b264b75
pterodactylscreams <pterodactylscreams@gmail.com>
2024-10-13 01:16:13 +0530

    hover effect changing colour added

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-let num=10
-const containerDiv=document.getElementById("containerDiv")
-containerDiv.innerHTML=""
+const button = document.getElementById("gridButton")
 
-let height = containerDiv.getBoundingClientRect().height;
-let width = containerDiv.getBoundingClientRect().width;
+button.addEventListener("click", function () {
+    let num = Number(prompt("Enter a value (limit: 100) for square grid dimensions:"))
+    while (num <= 0 || num > 101 || isNaN(num)) {
+        num = Number(prompt("Enter a value (limit: 100) for square grid dimensions:"))
+    }
+
+    const containerDiv = document.getElementById("containerDiv")
+    containerDiv.style.backgroundColor="#dcdbf4"
+    containerDiv.innerHTML = ""
+
+    let height = containerDiv.getBoundingClientRect().height;
+    let width = containerDiv.getBoundingClientRect().width;
 
 
-for (let i=0; i<num*num; i++){
-    const square=document.createElement("div")
-    square.classList.add("square")
+    for (let i = 0; i < num * num; i++) {
+        const square = document.createElement("div")
+        square.classList.add("square")
 
-    square.style.width=`${width/num}px`
-    square.style.height=`${height/num}px`
+        square.style.width = `${width / num}px`
+        square.style.height = `${height / num}px`
 
-    containerDiv.appendChild(square)
-}
+        containerDiv.appendChild(square)
+
+        square.addEventListener("mouseover", function(){
+            square.style.backgroundColor="#45327a1e"
+        })
+    }
+})
